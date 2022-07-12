52d6ede9d76581dae921abd4d6faa128b281ece1
Jacob Lopez <lopezjacob2003.jl@gmail.com>
2022-07-12 14:44:58 -0600

    Inital Commit

---
+++ src/components/button.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({color, text}) => {
+    return (
+        <div>
+            <button style={{ backgroundColor: color}} className="btn">{text}</button>
+        </div>
+    );
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onclick: PropTypes.func,
+}
+
+
+export default Button;
